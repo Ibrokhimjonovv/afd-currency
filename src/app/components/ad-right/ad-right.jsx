8943ed43e4fb902458e6196dc338207a66dda4ca
Ibrokhimjonovv <megabytekekabyte@gmail.com>
2025-08-14 13:55:32 +0500

    .

---
+++ src/app/components/ad-right/ad-right.jsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { useEffect } from 'react';
+import "./ad-right.scss";
+
+
+const AdRight = () => {
+    useEffect(() => {
+    const loadYandexAds = () => {
+      window.yaContextCb = window.yaContextCb || [];
+      window.yaContextCb.push(() => {
+        window.Ya.Context.AdvManager.render({
+          blockId: "R-A-16590718-1",
+          renderTo: "yandex_rtb_R-A-16590718-1",
+        });
+      });
+    };
+
+    if (!document.getElementById("yandex-ads-script")) {
+      const script = document.createElement("script");
+      script.id = "yandex-ads-script";
+      script.src = "https://yandex.ru/ads/system/context.js";
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = loadYandexAds;
+    } else {
+      loadYandexAds();
+    }
+  }, []);
+  return (
+    <div className='add-right'>
+        <div id="yandex_rtb_R-A-16590718-1"></div>
+    </div>
+  )
+}
+
+export default AdRight
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import AdLeft from "./components/ad-left/ad-left";
+import AdRight from "./components/ad-right/ad-right";
 import Calculate from "./components/calculate/calculate";
 
 export default function Home() {
   return (
     <div>
+      <AdLeft />
       <Calculate />
+      <AdRight />
     </div>
   );
 }
